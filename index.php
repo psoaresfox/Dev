d46c45c349fa262cb219ab46649d660cc2f5cd2e
psoaresfox <137503998+psoaresfox@users.noreply.github.com>
2023-08-09 22:06:34 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php 
+$num1 = 4;
+$num2 = 5;
+$media = ($num1 + $num2)/2;
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>paginaphp</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: black;
+            color: white;
+        }
+
+        header {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <div class="tab">
+            <ul>
+                <li>php</li>
+                <li>htm l</li>
+                <li>css</li>
+                <li>js</li>
+            </ul>
+        </div>
+    </header>    
+
+    <section>
+        <h1>Primiro site em php</h1>
+    </section>
+<script>
+
+</script>
+</body>
+</html>
